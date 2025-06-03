0c731f6fb31301ed0a91bb0982b1082a3bcf76bb
Yadjian92 <10462@holbertonstudents.com>
2025-06-03 09:12:33 +0200

    I did many task of the first project of this week. Exactly task 0 to 11.

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        
+        new_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                new_dict[attr] = getattr(self, attr)
+        return new_dict
+    
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import json
+
+def load_from_json_file(filename):
+    with open(filename, "r", encoding ='utf8') as my_file:
+        return json.load(my_file)
+    
