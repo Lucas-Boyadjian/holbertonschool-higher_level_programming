021285b5a0b567055e2981cdca1fd1c55f7be08d
Yadjian92 <10462@holbertonstudents.com>
2025-06-04 09:32:36 +0200

    Change file permissions for main and Pascal triangle modules; add implementation for Pascal's triangle function

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
-    
+    """Returns Pascal's triangle up to n rows"""
+    if n <= 0:
+        return []
+
+    result_list = []
+
+    for i in range(n):
+        actual_list = []
+
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                actual_list.append(1)
+            else:
+                prev_list = result_list[i - 1]
+                actual_list.append(prev_list[j - 1] + prev_list[j])
+
+        result_list.append(actual_list)
+
+    return result_list
