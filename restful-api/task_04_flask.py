c2fdeca31d13ca4606a462558db3c5ee34102b59
Yadjian92 <10462@holbertonstudents.com>
2025-06-10 16:52:36 +0200

    Implement a simple Flask application with home and data endpoints

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask AP!'
+
+@app.route('/data')
+def data():
+    users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+    return jsonify(users) 
+
+if __name__ == "__main__": app.run()
+
