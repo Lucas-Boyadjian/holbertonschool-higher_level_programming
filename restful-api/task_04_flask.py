75e6f0e62e7c2be2006bc1edab42b7502a157c84
Yadjian92 <10462@holbertonstudents.com>
2025-06-11 10:10:05 +0200

    Fix welcome message typo and update data endpoint to return user names

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,12 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the Flask AP!'
+    return 'Welcome to the Flask API!'
 
 @app.route('/data')
 def data():
     users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
-    return jsonify(users) 
+    
+    list_names = []
+    for user in users.values():
+        list_names.append(user["name"])
+    
+    return jsonify(list_names) 
 
 if __name__ == "__main__": app.run()
 
