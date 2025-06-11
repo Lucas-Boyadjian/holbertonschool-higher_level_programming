6c4ea89ac0fbd98c94f25fe18d0f37dcaf697acd
Yadjian92 <10462@holbertonstudents.com>
2025-06-11 11:19:49 +0200

    Fix HTTPServer reference in task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -70,5 +70,5 @@ class Serveur(http.server.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     server_address = ('', 8000)
-    httpd = HTTPServer(server_address, Serveur)
+    httpd = http.server.HTTPServer(server_address, Serveur)
     httpd.serve_forever()
