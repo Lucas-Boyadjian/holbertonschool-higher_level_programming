6d6c97df519533549a69de2ff58bf2144cecea7e
Yadjian92 <10462@holbertonstudents.com>
2025-06-25 17:04:49 +0200

    Update script to print the first State object from the hbtn_0e_6_usa database

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
+Script that prints the first State object from the database hbtn_0e_6_usa
 """
 from sys import argv
 from model_state import Base, State
@@ -15,4 +15,11 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
+    state = session.query(State).order_by(State.id).first()
+
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    session.close()
