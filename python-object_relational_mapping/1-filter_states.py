58758b7637d1599892af976701c4079465e5f0e0
Yadjian92 <10462@holbertonstudents.com>
2025-06-25 12:22:57 +0200

    Refactor SQL queries to enforce case sensitivity in state name filtering

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cur.execute(query)
-
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    
     rows = cur.fetchall()
 
     for row in rows:
