d8bec6530e030f6f4e6254fdefbc0683d1849fbb
Yadjian92 <10462@holbertonstudents.com>
2025-06-25 12:14:10 +0200

    Remove unnecessary blank lines for improved code cleanliness

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
 
     query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cur.execute(query)
-    
+
     rows = cur.fetchall()
-    
+
     for row in rows:
         print(row)
 
