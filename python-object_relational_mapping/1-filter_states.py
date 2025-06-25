7e50e1c13771865913f3f96160e5d10526862f89
Yadjian92 <10462@holbertonstudents.com>
2025-06-25 12:13:53 +0200

    Refactor SQL query execution for improved readability

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,9 +18,11 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cur.execute(query)
+    
     rows = cur.fetchall()
+    
     for row in rows:
         print(row)
 
