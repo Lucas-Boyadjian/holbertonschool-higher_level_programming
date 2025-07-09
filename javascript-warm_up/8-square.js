f158bbe3796e0ec15c3ffed46e69e45fd8612408
Yadjian92 <10462@holbertonstudents.com>
2025-07-09 13:30:46 +0200

    Fix control flow in 8-square.js to ensure proper execution of the square printing logic

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const SizeSquare = parseInt(process.argv[2]);
 
 if (isNaN(SizeSquare)) {
   console.log('Missing size');
-}
-
-for (let i = 0; i <= SizeSquare; i++) {
-  console.log('X'.repeat(SizeSquare));
+} else {
+  for (let i = 0; i <= SizeSquare; i++) {
+  	console.log('X'.repeat(SizeSquare));
+  }
 }
