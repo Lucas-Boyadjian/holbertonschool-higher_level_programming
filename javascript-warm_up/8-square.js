8d4f6bdd03fb570ac718be4259ed0c208b800e1a
Yadjian92 <10462@holbertonstudents.com>
2025-07-09 13:33:22 +0200

    Fix indentation in 8-square.js for consistency

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (isNaN(SizeSquare)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < SizeSquare; i++) {
-  	console.log('X'.repeat(SizeSquare));
+    console.log('X'.repeat(SizeSquare));
   }
 }
