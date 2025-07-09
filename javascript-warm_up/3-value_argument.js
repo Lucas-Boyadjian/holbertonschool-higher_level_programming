9d7728d5efb93a17f5021bda9970cfc96a10caf1
Yadjian92 <10462@holbertonstudents.com>
2025-07-09 11:53:37 +0200

    Refactor argument handling in 3-value_argument.js and add new scripts for concatenation and integer conversion

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 
-const count = process.argv.length - 2;
-
-if (count === 0) {
-    console.log('No argument')
-}
-else if (count === 1) {
-    console.log('Argument found')
-}    
-else {
-    console.log('Arguments found')
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
