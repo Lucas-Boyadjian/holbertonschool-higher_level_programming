6d33ab34d361a6875611178e88d83835d26c04d4
Yadjian92 <10462@holbertonstudents.com>
2025-07-09 13:31:54 +0200

    Fix loop condition in 8-square.js to prevent extra square printing

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const SizeSquare = parseInt(process.argv[2]);
 if (isNaN(SizeSquare)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= SizeSquare; i++) {
+  for (let i = 0; i < SizeSquare; i++) {
   	console.log('X'.repeat(SizeSquare));
   }
 }
