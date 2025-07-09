878fb5a94fcf13196a4f00bd9c4e31cce0a548a4
Yadjian92 <10462@holbertonstudents.com>
2025-07-09 12:13:23 +0200

    Refactor 5-to_integer.js to improve number parsing and logging

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (isNaN(Number(process.argv[2]))) {
+const num = parseInt(process.argv[2]);
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log(' My number: ' + Number(process.argv[2]));
+  console.log('My number: ' + num);
 }
