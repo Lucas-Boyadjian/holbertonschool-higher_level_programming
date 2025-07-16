ab0810b1298b23288798f9b719b6de28308a5cb7
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-07-16 22:07:12 +0200

    Add products and items data files, implement Flask routes for rendering items and products

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Flask application to render static and dynamic pages, including reading items from a JSON file
+and displaying them using Jinja templates.
+"""
+
+from flask import Flask, json, render_template, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Render the home page."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Render the about page."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Render the contact page."""
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    """Read items from JSON and render the items page."""
+    with open('items.json') as file:
+        items = json.load(file).get('items')
+    return render_template('items.html', items=items)
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    id = request.args.get('id')
+
+    if source == 'json':
+        with open('products.json') as file:
+            products = json.load(file)
+    
+    elif source == 'csv':
+        with open('products.csv') as file:
+            reader = csv.DictReader(file)
+            products = []
+            for row in reader:
+                products.append(row)
+
+    else:
+        render_template('product_display.html', error="Wrong source")
+    
+    if id:
+        products_id = filter(products, id)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
