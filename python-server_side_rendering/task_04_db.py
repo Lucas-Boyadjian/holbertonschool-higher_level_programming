7281b69da5b49f514d74febf7a2e7c5cc3749d81
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-07-17 18:38:33 +0200

    Implement database initialization and Flask route for product rendering from multiple sources

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Flask application to render static and dynamic product pages.
+Reads product data from JSON or CSV files and displays them using Jinja
+templates. Supports filtering by product id and error handling
+for invalid sources or missing products.
+"""
+
+from flask import Flask, json, render_template, request
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    id = request.args.get('id')
+
+    if source == 'json':
+        with open('products.json') as file:
+            products = json.load(file)
+
+    elif source == 'csv':
+        with open('products.csv') as file:
+            reader = csv.DictReader(file)
+            products = []
+            for row in reader:
+                products.append(row)
+    
+    elif source == 'sql':
+        con = sqlite3.connect('products.db')
+        cur = con.cursor()
+        
+
+    
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if id:
+        products_id = []
+        for index in products:
+            if str(index.get('id')) == str(id):
+                products_id.append(index)
+        products = products_id
+        if not products:
+            return render_template('product_display.html',
+                                   error="Product not found")
+    return render_template('product_display.html', products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
